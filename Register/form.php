88baa2526b2f294779235c20a32bacc6a2de7fb9
Daniel <Daniel.sabrkesh@outlook.com>
2018-11-07 14:23:46 +0100

    färdig

---
+++ Register/form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <form action="submit.php" method="POST">
+
+            Namn:
+            <input name="name" type="text"><br>
+
+            Födelsedatum
+            <input name="date" type="date"><br>
+
+            Mail:
+            <input name="mail" type="email"><br>
+
+            lösenord:
+            <input name="password" type="password"><br>
+
+            Mobilnummer 
+            <input name="phone" type="tel"><br>
+
+            Gender:
+            <input type="radio" name="gender" value="Man">Male 
+            <input type="radio" name="gender" value="kvinna"> Female
+
+            <input type="submit" value="Skicka">
+        </form>
+    </body>
+
